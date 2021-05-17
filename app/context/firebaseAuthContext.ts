ecf40309cc2465e032a915dc2f79197ac8d4485a
Ellisande <justin.dragos@gmail.com>
2021-05-16 20:17:07 -0700

    Remove contexts

--- app/context/firebaseAuthContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-import { auth } from "firebase";
-
-const FirebaseAuthContext = createContext<auth.Auth | undefined>(undefined);
-
-const useFirebaseAuth = () => useContext(FirebaseAuthContext);
-
-export { FirebaseAuthContext, useFirebaseAuth };
