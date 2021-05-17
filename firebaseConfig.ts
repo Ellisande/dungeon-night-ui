2703efc80eb1ad8ef9957e5f6b2a7cb364bcfbd3
Ellisande <justin.dragos@gmail.com>
2021-05-16 22:01:25 -0700

    Add back config.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,1 +1,8 @@
-export const config = {};
+export const config = {
+  apiKey: "AIzaSyB8laRFNWlGcuEWp66Du-4GAIgTBbWpzZE",
+  authDomain: "dungeon-night.firebaseapp.com",
+  projectId: "dungeon-night",
+  messagingSenderId: "62082775087",
+  appId: "1:62082775087:web:a5a535863690e68ed12eda",
+  measurementId: "G-G1E5691Z0J",
+};
